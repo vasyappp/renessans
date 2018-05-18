d161f63cd8d0812ba26245b87d5ef9ddb756ecad
Natalia Litvinova <tasmantia@gmail.com>
2018-05-18 17:27:52 +0300

    Исправлен метод StepCheckResult

--- src/main/java/Steps/CalculatorResultsSteps.java
+++ src/main/java/Steps/CalculatorResultsSteps.java
@@ -3,6 +3,8 @@ package Steps;
 import Pages.CalculatorResultsPage;
 import io.qameta.allure.Step;
 import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Описание шагов теста для страницы результатов калькулятора
@@ -11,19 +13,15 @@ public class CalculatorResultsSteps {
 
     @Step("поле {0} равно {1}")
     public void stepCheckResult(String fieldName, String expectedResult) {
-        String actualResult = new CalculatorResultsPage().getResult(fieldName);
-
-        if (!actualResult.equalsIgnoreCase(expectedResult))
-            try {
-                Thread.sleep(5000);
-                actualResult = new CalculatorResultsPage().getResult(fieldName);
-            } catch (InterruptedException e) {
-                Assert.fail("Interrupted");
-            }
+        WebDriverWait wait = new WebDriverWait(BaseSteps.getDriver(), 10);
 
         Assert.assertTrue(String.format
-                ("В поле %s рассчитано значение %s. Ожидалось - %s", fieldName, actualResult, expectedResult),
-                actualResult.equalsIgnoreCase(expectedResult));
+                ("В поле %s рассчитано значение %s. Ожидалось - %s", fieldName,
+                        new CalculatorResultsPage().getResult(fieldName), expectedResult),
+                wait.until((ExpectedCondition<Boolean>) driver -> {
+                    String actualResult = new CalculatorResultsPage().getResult(fieldName);
+                    return actualResult.equalsIgnoreCase(expectedResult);
+                }));
     }
 
 }
